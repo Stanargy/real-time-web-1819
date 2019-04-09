048407c77ef0fe5df50aaba896ca466c98d74ebd
Stan Ooms <35112551+Stanargy@users.noreply.github.com>
2019-04-09 12:09:54 +0200

    socket.io init

--- MyProject/index.js
+++ MyProject/index.js
@@ -5,6 +5,7 @@
     const express = require('express');
     const router = require('./router/router')
     const app = express()
+    const io = require('../../')(server);
 
 
     // setup template engine
@@ -19,8 +20,65 @@ app.use('/', router)
 
  // listen to port
  console.log('PORT 3000 | Server Started')
- app.listen(8080);
+ app.listen(3000);
 
+// chat
 
+let numUsers = 0;
+
+io.on('connection', (socket) =>{
+    let addedUser = false;
+
+    // when the client emits a new message, this block listens and executes
+    socket.on('new message', (data) =>{
+        // tell the client to execute new message
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
+    });
+
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', (username) =>{
+        if (addedUser) return;
+
+        // store username in socket session for this client
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+
+        });
+    });
+
+    // when client starts typing we broadcast it to others
+    socket.on('typing', ()=>{
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
+
+    // when client stops typing we broadcast it to the user
+    socket.on('stop typing', () =>{
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
+
+    // whem the user disconnects.. perform this block of code:
+    socket.on('disconnect', () =>{
+        if (addedUser) {
+            --numUsers;
+
+            // echo globally that this client has left
+
+            socket.breadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
+});
 
 }())
