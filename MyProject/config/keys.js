523125147faac5b603583b9bfd92e91cd88f7aaf
Stan Ooms <35112551+Stanargy@users.noreply.github.com>
2019-04-12 13:13:55 +0200

    deploy

---
+++ MyProject/config/keys.js
@@ -0,0 +1,15 @@
+// add this file to .gitignore
+module.exports = {
+    google: {
+        clientID:'600622886294-qnclq5nprn927mesq8c326d4chh5j105.apps.googleusercontent.com',
+        clientSecret:'JFjaIWtfWsMfCmeXXZR4GQCZ'
+    },
+    mongodb:{
+        // dbURI: 'http://api.mongolab.com/api/1/databases/chat_app/collections/'
+        dbURI:'mongodb://admin:admin123@ds137605.mlab.com:37605/chat_app',
+        apiKey: 'hfUCcfvAcZeKoGPCd0TTy5K5ZfQk_UMP'
+    },
+    session: {
+        cookieKey: 'uoewbkdsvijshjksdfkjdsfi'
+    }
+};
